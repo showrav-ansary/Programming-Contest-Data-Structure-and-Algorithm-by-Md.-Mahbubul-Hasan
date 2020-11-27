0426cfa436f1c0ee6beb478ffa70727bd2c12373
showrav-ansary <showrav.ansary.bd@gmail.com>
2020-11-27 06:42:22 +0600

    Timus -> 4

---
+++ Timus/1000.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int operand1, operand2;
+    scanf("%d %d", &operand1, &operand2);
+    printf("%d\n", operand1 + operand2);
+    return 0;
+}
---
+++ Timus/1293.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, a, b;
+    scanf("%d %d %d", &n, &a, &b);
+    printf("%d \n", 2*a*b*n);
+    return 0;
+}
