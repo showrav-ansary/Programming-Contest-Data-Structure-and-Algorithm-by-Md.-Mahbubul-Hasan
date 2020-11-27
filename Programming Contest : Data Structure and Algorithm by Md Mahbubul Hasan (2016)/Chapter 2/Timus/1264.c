d9b0e7b9ea3f50256e6a675c466683cdcfc5d4a5
showrav-ansary <showrav.ansary.bd@gmail.com>
2020-11-28 04:52:39 +0600

    Removed

--- Programming Contest : Data Structure and Algorithm by Md Mahbubul Hasan (2016)/Chapter 2/Timus/1264.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int operand1, operand2;
-    scanf("%d %d", &operand1, &operand2);
-    printf("%d\n", operand1*(++operand2));
-    return 0;
-}
--- Programming Contest : Data Structure and Algorithm by Md Mahbubul Hasan (2016)/Chapter 2/Timus/1293.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int n, a, b;
-    scanf("%d %d %d", &n, &a, &b);
-    printf("%d \n", 2*a*b*n);
-    return 0;
-}
