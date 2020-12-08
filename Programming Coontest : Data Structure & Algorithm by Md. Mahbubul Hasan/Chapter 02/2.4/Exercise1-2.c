3b48359855beafa1194d8d777df2d8909adfd2a0
showrav-ansary <showrav.ansary.bd@gmail.com>
2020-12-08 10:26:38 +0600

    2.4 - Formula Solutions Commented - 1.1, 1.2

--- Programming Coontest : Data Structure & Algorithm by Md. Mahbubul Hasan/Chapter 02/2.4/Exercise1-2.c
+++ Programming Coontest : Data Structure & Algorithm by Md. Mahbubul Hasan/Chapter 02/2.4/Exercise1-2.c
@@ -9,5 +9,8 @@ int main()
     for (int iterator = 1; iterator <= n; iterator++)
         sum += iterator*iterator;
     printf("%d\n", sum);
+    /*
+    formula solution : printf("%d\n", n*(n+1)*(2n+1)/6)
+    */
     return 0;
 }
