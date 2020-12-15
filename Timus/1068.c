91ddf344cc2f0e1a2948ea4b9adb29070aa259e3
showrav-ansary <showrav.ansary.bd@gmail.com>
2020-12-15 15:26:23 +0600

    Timu1068 added!

--- Timus/1068.c
+++ Timus/1068.c
@@ -5,8 +5,11 @@ int main()
     int n;
     scanf("%d", &n);
     if (fabs(n) <= 10000)
-        if (n >= 0)
+        if (n > 0)
             printf("%d",n*(n+1)/2);
+        else if (n == 0)
+            printf("1");
         else
             printf("%d",(2-n)*(n+1)/2);
+    return 0;
 }
