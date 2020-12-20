81af1ae7737aa9458f5e1af6cef2e6c7b06f4a4f
showrav-ansary <showrav.ansary.bd@gmail.com>
2020-12-20 23:34:00 +0600

    Sieve implemented

--- Programming Contest : Data Structure & Algorithm by Md. Mahbubul Hasan/Chapter 02/2.4/Timus/1086.c
+++ Programming Contest : Data Structure & Algorithm by Md. Mahbubul Hasan/Chapter 02/2.4/Timus/1086.c
@@ -1,1 +1,24 @@
 //Sieve's Algorithm has been explained in the next chapter. Follow that!
+int Prime[300000], nPrime;
+
+int mark[1000002];
+
+void sieve(int n)
+{
+    int i, j, limit = sqrt(n * 1.0) + 2;
+    mark[i] = 1;
+
+    for (i = 4; i <= n; i += 2)
+        mark[i] = 1;
+
+    Prime[nPrime++] = 2;
+
+    for (i = 3; i <= n; i += 2)
+        if (!mark[i])
+        {
+            Prime[nPrime++] = i;
+            if (i <= limit)
+                for (j = i * i; j <= n; j += i * 2)
+                    mark[j] = 1;
+        }
+}
