9c75e426c49f02bd06f01ab3ba36d2265b5caabd
showrav-ansary <showrav.ansary.bd@gmail.com>
2021-07-16 02:22:03 +0600

    Chapter 2.5 upto Exercise 9

---
+++ Chapter 02/2.5/Exercise5.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Exercise5{
+
+    // this array will keep track of cumulative sum
+    private static int sumArray;
+
+    public static void main(String[] args) {
+         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+         System.out.println("Enter the number of inputs : ");
+         int length = in.nextInt();
+
+         int[] sumArray = new int[length];
+
+         System.out.println("Enter a number : ");
+         sumArray[0] = in.nextInt();
+
+         for(int i = 1; i < length; i++){
+            System.out.println("Enter a number : ");
+            sumArray[i] = in.nextInt() + sumArray[i-1];
+         }
+
+         System.out.println("Enter the value of i (starting from 0) : ");
+         int i = in.nextInt();
+
+         System.out.println("Enter the value of j (starting from 0) : ");
+         int j = in.nextInt();
+
+         int result = 0;
+
+         if(i == 0)
+            result = sumArray[j];
+         else
+            result = sumArray[j] - sumArray[i-1];
+
+         System.out.println("Result is : " + result);
+    }
+}
---
+++ Chapter 02/2.5/Exercise6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Exercise6{
+
+    private static int GetLengthOfStringWithCharArray(String string){
+        int length = 0;
+        char[] charArray = string.toCharArray();
+        for(char character : charArray){
+            length++;
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+         Scanner in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+         System.out.println("Enter the input : ");
+         String input = in.nextLine();
+
+         System.out.println("Result is (using `.length()`) : " + input.length());
+         System.out.println("Result is (custom method) : " + GetLengthOfStringWithCharArray(input));
+    }
+}
